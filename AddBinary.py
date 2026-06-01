5547f91dc170487c238b73c5162dbc31aa1f1b24
WitchesBr3w <robloxbypassesdiscord@gmail.com>
2026-06-01 11:36:46 -0400

    Create AddBinary.py
    The solution to the 67th leetcode problem (Add Binary) in which we use three functions: one to add the binary numbers, one to convert the binary numbers into a decimal number and one to convert the number into a binary number
    after we convert our two binary numbers into decimals, we then add them together and then convert them back into binary

---
+++ AddBinary.py
@@ -0,0 +1,30 @@
+class Solution:
+    def binary_to_decimal(self, binary)
+        decimal = 0
+        power = 0
+        
+        for digit in reversed(binary):
+            if digit == '1':
+                decimal += 2 ** power
+            power += 1
+            
+        return decimal
+
+    def decimal_to_binary(self, num) 
+        if num == 0:
+            return "0"
+        
+        result = ""
+        while num > 0:
+            result = str(num % 2) + result
+            num //= 2
+            
+        return result
+
+    def addBinary(self,a,b)
+        dec_a = self.binary_to_decimal(a)
+        dec_b = self.binary_to_decimal(b)
+        
+        total = dec_a + dec_b
+        
+        return self.decimal_to_binary(total)
